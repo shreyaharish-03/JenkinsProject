a3300ce33fcd2c0391a4f936347431dd3a324dfc
shreyaharish-03 <shreyaharish84@gmail.com>
2026-06-24 12:15:06 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add 
+{
+    public static void main( String[] args )
+    {
+        int a=20,b=15,sum;
+        sum = a + b;
+        System.out.println("The sum od the numbers" + "a=" + a + "b=" + b + "is" + sum ".");
+    }
+}
