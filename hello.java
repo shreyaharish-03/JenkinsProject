afa2129a212ce195b7eb75b262a18c947c269a7a
shreyaharish-03 <shreyaharish84@gmail.com>
2026-06-24 12:19:11 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,10 +2,8 @@ public class hello
 {
     public static void main( String[] args )
     {
-        int a=5,b=10;
-        a = a + b;
-        b = a - b;
-        a = a - b;
-        System.out.println( "a=" + a + "b=" + b );
+        int a=5,b=10,c=0;
+        c = a + b;
+        System.out.println( "sum" + c );
     }
 }
